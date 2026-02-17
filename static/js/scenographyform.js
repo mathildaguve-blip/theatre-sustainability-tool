1f4be6969d9e50e51097ef54d53d761cc5e247a0
mathildaguve-blip <mathildaguve@gmail.com>
2026-02-17 13:55:14 +0100

    Update scenographyform.js

--- static/js/scenographyform.js
+++ static/js/scenographyform.js
@@ -1,1 +1,18 @@
+export function ScenographyForm() {
+    const div = document.createElement('div');
+    div.innerHTML = `
+        <h2>Scenografi</h2>
+        <label>Material:</label>
+        <input type="text" placeholder="Sök material..." id="sceno-material">
+        <label>Butik:</label>
+        <input type="text" placeholder="Sök butik..." id="sceno-shop">
+        <label>Fraktland:</label>
+        <input type="text" placeholder="Sök land..." id="sceno-country">
+        <label>Färgning / Patinering:</label>
+        <input type="text" placeholder="Exempel: kaffe, naturfärg..." id="sceno-patina">
+        <label>Tvätt:</label>
+        <input type="text" placeholder="Exempel: 30°C, handtvätt..." id="sceno-wash">
+    `;
+    return div;
+}
 
