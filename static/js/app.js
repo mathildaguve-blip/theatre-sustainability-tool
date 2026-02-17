e22bd11f70ed06659d152625eb3fb155fc0fa383
mathildaguve-blip <mathildaguve@gmail.com>
2026-02-17 13:54:08 +0100

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,16 @@
+import { CostumeForm } from './CostumeForm.js';
+import { MaskForm } from './MaskForm.js';
+import { ScenographyForm } from './ScenographyForm.js';
+
+export function App() {
+    const container = document.createElement('div');
+
+    const steps = document.createElement('div');
+    steps.appendChild(CostumeForm());
+    steps.appendChild(MaskForm());
+    steps.appendChild(ScenographyForm());
+
+    container.appendChild(steps);
+    return container;
+}
 
